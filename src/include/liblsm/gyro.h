c8daae38d174d29c66996af96a8df324a71d4a31
kangaruppel <emily.k.ruppel@gmail.com>
2019-11-01 18:39:24 -0400

    Added extra functions for initializing just gyro or just accel functionality with odr input

--- src/include/liblsm/gyro.h
+++ src/include/liblsm/gyro.h
@@ -46,6 +46,8 @@ void gyro_init_raw(void);
 void gyro_init_data_rate(LSM6DS3_ACC_GYRO_ODR_XL_t);
 void gyro_only_init_data_rate(LSM6DS3_ACC_GYRO_ODR_XL_t rate);
 void gyro_init_data_rate_hm(LSM6DS3_ACC_GYRO_ODR_XL_t, bool hm);
+void gyro_only_init_odr_hm(LSM6DS3_ACC_GYRO_ODR_XL_t rate, bool highperf);
+void accel_only_init_odr_hm(LSM6DS3_ACC_GYRO_ODR_XL_t rate, bool highperf);
 void gyro_init_tap_drdy(void);
 void gyro_init_fifo_tap(void);
 
